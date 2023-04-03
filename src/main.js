63b9335883f90037a2aa570c76e9153f118f6a1e
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2023-04-03 20:31:05 +0400

    feat: Базовая разметка контейнера

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import App from '@/app/App.vue';
 import '@/assets/styles/main.sass';
 
-createApp(App).mount('#app');
+createApp(App).mount('#widget');
