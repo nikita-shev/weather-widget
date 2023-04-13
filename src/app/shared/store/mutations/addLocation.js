9976268b61fe92f64d17c395945022fc7f4005a4
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2023-04-13 19:23:04 +0400

    fix: В addLocation() добавлены свойства "id" и "order"

--- src/app/shared/store/mutations/addLocation.js
+++ src/app/shared/store/mutations/addLocation.js
@@ -1,8 +1,11 @@
 export default function ADD_LOCATION(state, payload) {
-   const { country, city, data } = payload;
-   const location = state.find((item) => item.data.id === data.id);
+   const location = state.find((item) => item.data.id === payload.data.id);
 
    if (!location) {
-      state.push({ country, city, data });
+      const data = { id: payload.data.id, order: null, ...payload };
+      const latestData = state.at(-1);
+
+      data.order = latestData ? latestData.order + 1 : 1;
+      state.push(data);
    }
 }
