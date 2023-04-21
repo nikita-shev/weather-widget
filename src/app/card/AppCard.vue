ecc346b7e8e09b8d8127b67277a0f44cd02f2cf5
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2023-04-21 06:21:20 +0400

    feat: Визуализация погодных данных

--- src/app/card/AppCard.vue
+++ src/app/card/AppCard.vue
@@ -3,12 +3,12 @@
       <header class="card__card-header card-header">
          <div class="card-header__wrap">
             <img
-               :src="`https://flagcdn.com/16x12/countryCode.webp`"
-               alt="country"
+               :src="`https://flagcdn.com/16x12/${countryCode}.webp`"
+               :alt="country"
                class="card-header__img"
             />
 
-            <h2 class="card-header__title">country, city</h2>
+            <h2 class="card-header__title">{{ country }}, {{ city }}</h2>
          </div>
 
          <remove-card-btn class="card-header__btn" />
@@ -18,44 +18,44 @@
          <div class="weather-data__temp temp">
             <div class="temp__info">
                <img
-                  :src="`https://openweathermap.org/img/wn/weather.icon@2x.png`"
+                  :src="`https://openweathermap.org/img/wn/${weather.icon}@2x.png`"
                   class="temp__img"
                   alt="Температура"
                />
 
-               <p class="temp__text">temp°C</p>
+               <p class="temp__text">{{ temp }}°C</p>
             </div>
 
             <div class="temp__notes">
-               <p class="temp__feels-like">Ощущается как feelsLike°C.</p>
-               <p class="temp__clouds">weather.description.</p>
+               <p class="temp__feels-like">Ощущается как {{ feelsLike }}°C.</p>
+               <p class="temp__clouds">{{ weather.description }}.</p>
             </div>
          </div>
 
          <div class="weather-data__other-data other-data">
             <div class="other-data__wrap">
                <i-pressure class="other-data__img" />
-               <span class="other-data__text">pressure мм рт.ст.</span>
+               <span class="other-data__text">{{ pressure }} мм рт.ст.</span>
             </div>
 
             <div class="other-data__wrap">
                <i-humidity class="other-data__img" />
-               <span class="other-data__text">humidity %</span>
+               <span class="other-data__text">{{ humidity }} %</span>
             </div>
 
             <div class="other-data__wrap">
                <i-visibility class="other-data__img" />
-               <span class="other-data__text">visibility км</span>
+               <span class="other-data__text">{{ visibility }} км</span>
             </div>
 
             <div class="other-data__wrap">
                <i-wind-speed class="other-data__img" />
-               <span class="other-data__text">windSpeed м/с</span>
+               <span class="other-data__text">{{ windSpeed }} м/с</span>
             </div>
          </div>
       </div>
 
-      <img src="bgUrl" alt="weather.main" class="card__bg" />
+      <img :src="bgUrl" :alt="weather.main" class="card__bg" />
    </article>
 </template>
 
@@ -65,6 +65,7 @@
    import IHumidity from '@/app/card/Icons/IHumidity.vue';
    import IVisibility from '@/app/card/Icons/IVisibility.vue';
    import IWindSpeed from '@/app/card/Icons/IWindSpeed.vue';
+   import { computed, toRefs } from 'vue';
 
    export default {
       name: 'AppCard',
@@ -75,6 +76,48 @@
          IVisibility,
          IWindSpeed,
          RemoveCardBtn
+      },
+
+      props: {
+         weatherData: {
+            type: Object,
+            required: true,
+            default() {
+               return { data: { weather: {}, main: {} } };
+            }
+         }
+      },
+
+      setup(props) {
+         const { country, city, data } = toRefs(props.weatherData);
+         const countryCode = data.value.sys.country.toLowerCase();
+
+         const temp = Math.round(data.value.main.temp);
+         const feelsLike = Math.round(data.value.main.feels_like);
+         const pressure = Math.round(data.value.main.pressure * 0.750064);
+         const visibility = data.value.visibility / 1000;
+
+         const bgUrl = computed(
+            () =>
+               new URL(
+                  `/src/assets/images/weather/${data.value.weather[0].main.toLowerCase()}.jpg`,
+                  import.meta.url
+               ).pathname
+         );
+
+         return {
+            bgUrl,
+            country,
+            countryCode,
+            city,
+            weather: data.value.weather[0],
+            temp,
+            feelsLike,
+            pressure,
+            humidity: data.value.main.humidity,
+            visibility,
+            windSpeed: data.value.wind.speed
+         };
       }
    };
 </script>
